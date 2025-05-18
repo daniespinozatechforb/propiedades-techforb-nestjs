ca0edf9cad4b30f6d4fc85be245a98bc356da37d
DaniEspinoza00 <jorgedanielespinoza00@gmail.com>
2025-05-18 19:54:04 -0300

    Arreglo de problema 'Delete eslintprettier/prettier'.

--- src/imagen/imagen.service.ts
+++ src/imagen/imagen.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateImagenDto } from './dto/create-imagen.dto';
-import { UpdateImagenDto } from './dto/update-imagen.dto';
 import { Imagen } from './entities/imagen.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Propiedad } from 'src/propiedad/entities/propiedad.entity';
@@ -16,24 +15,6 @@ export class ImagenService {
     @InjectRepository(Propiedad)
     private readonly propiedadRepository: Repository<Propiedad>,
   ) { }
-
-  /*   async create(createImagenDto: CreateImagenDto): Promise<Imagen> {
-    const propiedad = await this.propiedadRepository.findOne({
-      where: { id: createImagenDto.propiedadId },
-      relations: ['imagenes'],
-    });
-  
-    if (!propiedad) {
-      throw new NotFoundException('Propiedad no encontrada');
-    }
-  
-    const imagen = this.imagenRepository.create({
-      url: createImagenDto.url,
-      propiedad,
-    });
-  
-    return this.imagenRepository.save(imagen);
-  } */
 
   async addImagenToPropiedad(propiedadId: number, dto: CreateImagenDto): Promise<Imagen> {
     const propiedad = await this.propiedadRepository.findOne({
