0d9115a605c4c551500e3bca33af3af8aa96463c
DaniEspinoza00 <jorgedanielespinoza00@gmail.com>
2025-05-28 11:07:42 -0300

    cambio en los exports de localidad y provincia por crasheo de aplicacion

--- src/provincia/provincia.module.ts
+++ src/provincia/provincia.module.ts
@@ -8,5 +8,6 @@ import { Provincia } from './entities/provincia.entity';
   imports: [TypeOrmModule.forFeature([Provincia])],
   controllers: [ProvinciaController],
   providers: [ProvinciaService],
+  exports: [ProvinciaService],
 })
 export class ProvinciaModule {}
